a4b565a811c327565e9da0d04ebc1f7e1bf830f9
Pammota <gligor.bogdan@campeni.ro>
2024-07-20 04:16:40 +0300

    added unique index

--- martini-be/models/models.go
+++ martini-be/models/models.go
@@ -2,7 +2,7 @@ package models
 
 type Item struct {
 	ID        string `json:"id" gorm:"primaryKey"`
-	Index     int    `json:"index" gorm:"index,unique"`
+	Index     int    `json:"index" sql:"AUTO_INCREMENT" gorm:"index;unique;autoIncrement"`
 	Name      string `json:"name"`
 	Completed bool   `json:"completed"`
 }
